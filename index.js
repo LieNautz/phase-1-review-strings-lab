9c28ec44258d430be00bb2ebf5eddd4040cd8af2
LieNautz <tuan_lai01@hotmail.com>
2022-07-31 18:50:39 +1000

    complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code in this file!
+// describe('index.js', () => {
+//     describe('currentUser', () => {
+//       it('is defined', () => {
+//         expect(currentUser, "The 'currentUser' variable must contain a string").to.be.a('string');
+//         expect(currentUser, "You need to modify the value of the 'currentUser' variable").to.not.be.empty;
+//       });
+//     });
+  
+//     describe('welcomeMessage', () => {
+//       it('contains "Welcome to Flatbook, "', () => {
+//         expect(welcomeMessage).to.have.string('Welcome to Flatbook, ');
+//       });
+  
+//       it("contains the value of the 'currentUser' variable", () => {
+//         expect(welcomeMessage).to.have.string(currentUser);
+//       });
+  
+//       it('ends with an exclamation point!', () => {
+//         expect(welcomeMessage.substr(-1)).to.eq('!');
+//       });
+//     });
+
+const currentUser = "John";
+
+const firstInitial = "J";
+
+const upperCaseCurrentUser= currentUser
+
+const welcomeMessage= "Welcome to Flatbook, " + currentUser + "!";
+
+const excitedWelcomeMessage= "WELCOME TO FLATBOOK, ", ${currentUser.toLocaleUpperCase()} ,"!"
+
+const shortGreeting = "Welcome, " + firstInitial + "!"
+
